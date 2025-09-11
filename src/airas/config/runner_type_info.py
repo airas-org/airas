32c04271a08770554f0c3ebb8db11d674dc15e6f
toma tanaka <ulti4929@gmail.com>
2025-09-11 12:54:58 +0000

    add A100*1 GPU

--- src/airas/config/runner_type_info.py
+++ src/airas/config/runner_type_info.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-RunnerType = Literal["ubuntu-latest", "Tesla_T4", "A100_80GM×8"]
+RunnerType = Literal["ubuntu-latest", "Tesla_T4", "A100_80GM×1", "A100_80GM×8"]
 
 runner_info_dict = {
     "ubuntu-latest": {
@@ -15,6 +15,13 @@ RAM：500 MB""",
 NVIDIA Tesla T4
 VRAM：16 GB
 RAM：500 MB""",
+    },
+    "A100_80GM×1": {
+        "runner_setting": '["self-hosted", "A100 80GM×1"]',
+        "prompt": """\
+NVIDIA A100
+VRAM：80GB
+RAM：2048 GB""",
     },
     "A100_80GM×8": {
         "runner_setting": '["self-hosted", "A100 80GM×8"]',
