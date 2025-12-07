a6d0423561e77af16fd65da4ecce5bf9ab5b440d
Developer <developer@example.com>
2025-12-07 12:09:02 +0000

    add latex push api

--- backend/api/schemas/latex.py
+++ backend/api/schemas/latex.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 from airas.types.latex import LATEX_TEMPLATE_NAME
 from airas.types.paper import PaperContent
+from src.airas.types.github import GitHubConfig
 
 
 class GenerateLatexSubgraphRequestBody(BaseModel):
@@ -12,4 +13,16 @@ class GenerateLatexSubgraphRequestBody(BaseModel):
 
 class GenerateLatexSubgraphResponseBody(BaseModel):
     latex_text: str
+    execution_time: dict[str, list[float]]
+
+
+class PushLatexSubgraphRequestBody(BaseModel):
+    github_config: GitHubConfig
+    latex_text: str
+    latex_template_name: LATEX_TEMPLATE_NAME = "iclr2024"
+
+
+class PushLatexSubgraphResponseBody(BaseModel):
+    is_upload_successful: bool
+    is_images_prepared: bool
     execution_time: dict[str, list[float]]
