a6117279e425cb0fdbdce6b66455f3d224d430ee
genga6 <gengaru617science@gmail.com>
2026-02-23 07:24:13 +0000

    remove old topic_open_ended

--- backend/src/airas/usecases/autonomous_research/topic_open_ended_research/topic_open_ended_research.py
+++ backend/src/airas/usecases/autonomous_research/topic_open_ended_research/topic_open_ended_research.py
@@ -180,6 +180,7 @@ class TopicOpenEndedResearch:
         runner_config: RunnerConfig,
         wandb_config: WandbConfig,
         task_id: UUID,
+        created_by: UUID,
         is_github_repo_private: bool = False,
         search_method: SearchMethod = "airas_db",
         search_index: AirasDbPaperSearchIndex | None = None,
@@ -207,6 +208,7 @@ class TopicOpenEndedResearch:
         self.runner_config = runner_config
         self.wandb_config = wandb_config
         self.task_id = task_id
+        self.created_by = created_by
         self.is_github_repo_private = is_github_repo_private
         self.num_paper_search_queries = num_paper_search_queries
         self.papers_per_query = papers_per_query
@@ -229,7 +231,7 @@ class TopicOpenEndedResearch:
         self.e2e_service.create(
             id=self.task_id,
             title="Untitled E2E Research Task",
-            created_by=UUID("00000000-0000-0000-0000-000000000001"),
+            created_by=self.created_by,
             status=Status.RUNNING,
             current_step=StepType.GENERATE_QUERIES,
             github_url=github_url,
