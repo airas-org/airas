d4c84e5156acc897c883d9705cc629f034b3e4f2
eycjur <63308909+eycjur@users.noreply.github.com>
2026-07-18 23:41:51 +0000

    Add frontend for reproduction

---
+++ frontend/src/lib/api/models/DispatchPaperReproductionGenerateRequestBody.ts
@@ -0,0 +1,22 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { DispatchPaperReproductionGenerateLLMMapping } from './DispatchPaperReproductionGenerateLLMMapping';
+import type { GitHubConfigRequest } from './GitHubConfigRequest';
+export type DispatchPaperReproductionGenerateRequestBody = {
+    github_config: GitHubConfigRequest;
+    paper_url: string;
+    instruction: string;
+    repo_url?: string;
+    github_actions_agent?: DispatchPaperReproductionGenerateRequestBody.github_actions_agent;
+    runner_label?: (Array<string> | null);
+    llm_mapping?: (DispatchPaperReproductionGenerateLLMMapping | null);
+};
+export namespace DispatchPaperReproductionGenerateRequestBody {
+    export enum github_actions_agent {
+        CLAUDE_CODE = 'claude_code',
+        OPEN_CODE = 'open_code',
+    }
+}
+
